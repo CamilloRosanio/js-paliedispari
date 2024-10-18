3830bd41b292551a431af957d5851b32d76c41e5
MyUsername <rosanio.c@gmail.com>
2024-10-18 16:57:59 +0200

    ESERCIZIO Parola Palindroma - creazione prompt + funzione Reverse

--- js/script.js
+++ js/script.js
@@ -1,11 +1,68 @@
 console.log('DEFAULT CONNECTION TEST')
 
-
-/*
+/* ESERCIZIO 1 - Parola Palindroma
 Palidroma
 Chiedere all’utente di inserire una parola
-Creare una funzione per capire se la parola inserita è palindroma
+Creare una funzione per capire se la parola inserita è palindroma */
 
+
+// Chiedo all'utente una stringa
+let userInput = prompt('Inserisci una parola palindroma');
+
+// Dichiaro la variabile del mio testo in reverse, per portarla fuori dalla funzione
+let reversedString;
+
+// Creo una funzione per ribaltare la stringa scelta dall'utente
+/**
+ * La funzione ribalta i caratteri di una stringa, posizionando l'ultimo carattere come primo e così via.
+ * @param {String} string String è il parametro esterno di tipo stringa da elaborare
+ */
+function reverseString(string) {
+    const reversedCharacters = [];
+
+    for (let i = string.length; i >= 0; i--) {
+        let extractedCharacter = string.charAt(i);
+    
+        reversedCharacters.push(extractedCharacter);
+    }
+    
+    reversedString = reversedCharacters.join('');
+    
+    return reversedString;
+    
+}
+
+// Utilizzo la funzione reverseString
+reverseString(userInput);
+
+// Stampo il risultato ottenuto come variabile
+let reversed_Userinput = reversedString;
+console.log(reversed_Userinput);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 Pari e Dispari
 L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
